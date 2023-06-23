96946a965cb659f61b6b0acfdd2f7ce2e9df7109
endogen <081081@gmx.net>
2023-06-23 11:41:14 +0200

    Simplified implementation

--- plg/about/about.py
+++ plg/about/about.py
@@ -1,6 +1,5 @@
 from plugin import TGBFPlugin
 from telegram import Update
-from telegram.constants import ParseMode
 from telegram.ext import CallbackContext, CommandHandler
 
 
@@ -11,6 +10,4 @@ class About(TGBFPlugin):
 
     @TGBFPlugin.send_typing
     async def about_callback(self, update: Update, context: CallbackContext):
-        await update.message.reply_text(
-            text="WORKS",
-            parse_mode=ParseMode.MARKDOWN_V2)
+        await update.message.reply_text(text="WORKS")
