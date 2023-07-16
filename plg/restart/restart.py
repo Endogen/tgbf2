42b9dbf63b3b7bafe40fa067ccfa69d9f7485993
endogen <081081@gmx.net>
2023-07-17 00:53:59 +0200

    Reworked 'restart' command, but it's still not working

---
+++ plg/restart/restart.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import logging
+import asyncio
+import emoji as emo
+
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+from plugin import TGBFPlugin
+
+
+class Restart(TGBFPlugin):
+
+    async def init(self):
+        await self.add_handler(CommandHandler(self.handle, self.init_callback, block=False))
+
+        chat_id = self.cfg.get("chat_id")
+        mess_id = self.cfg.get("message_id")
+
+        # If no data saved, don't do anything
+        if not mess_id or not chat_id:
+            return
+
+        try:
+            await self.tgb.app.bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=mess_id,
+                text=f"{emo.DONE} Restarting bot..."
+            )
+        except Exception as e:
+            logging.error(str(e))
+        finally:
+            self.cfg.remove("chat_id")
+            self.cfg.remove("message_id")
+
+    @TGBFPlugin.owner
+    @TGBFPlugin.private
+    @TGBFPlugin.send_typing
+    async def init_callback(self, update: Update, context: CallbackContext):
+        msg = await update.message.reply_text(f"{emo.HOURGLASS} Restarting bot...")
+
+        chat_id = msg.chat_id
+        mess_id = msg.message_id
+
+        self.cfg.set(chat_id, "chat_id")
+        self.cfg.set(mess_id, "message_id")
+
+        m_name = __spec__.name
+        m_name = m_name[:m_name.index(".")]
+
+        await asyncio.sleep(1)
+        os.execl(sys.executable, sys.executable, '-m', m_name, *sys.argv[1:])
