4efa759d27b7ab37b223490df6f4f280c33847ff
endogen <081081@gmx.net>
2023-07-16 22:14:16 +0200

    Method name changed

--- plg/database/database.py
+++ plg/database/database.py
@@ -17,7 +17,7 @@ class Database(TGBFPlugin):
     @TGBFPlugin.send_typing
     async def init_callback(self, update: Update, context: CallbackContext):
         if len(context.args) < 2:
-            await update.message.reply_text(await self.get_usage())
+            await update.message.reply_text(await self.get_plg_info())
             return
 
         sub_command = context.args[0].lower()
