11717cf858b29b62e4408f8919191dd1a5c4346b
endogen <081081@gmx.net>
2023-07-17 00:07:40 +0200

    Finished 'logfile' command

---
+++ plg/logfile/logfile.py
@@ -0,0 +1,36 @@
+import logging
+import emoji as emo
+import constants as con
+
+from pathlib import Path
+from telegram import Update
+from plugin import TGBFPlugin
+from telegram.ext import CallbackContext, CommandHandler
+
+
+class Logfile(TGBFPlugin):
+
+    async def init(self):
+        await self.add_handler(CommandHandler(self.handle, self.init_callback, block=False))
+        await self.add_handler(CommandHandler('log', self.init_callback, block=False))
+
+    @TGBFPlugin.owner
+    @TGBFPlugin.private
+    @TGBFPlugin.send_typing
+    async def init_callback(self, update: Update, context: CallbackContext):
+        log_file = max(con.DIR_LOG.glob('*.log'), key=lambda item: item.stat().st_ctime)
+
+        if Path.is_file(Path(log_file)):
+            try:
+                file = open(log_file, 'rb')
+            except Exception as e:
+                file = None
+                logging.error(e)
+                await self.notify(e)
+        else:
+            file = None
+
+        if file:
+            await update.message.reply_document(document=file)
+        else:
+            await update.message.reply_text(f"{emo.WARNING} No logfile found")
